c67936e176560fd0b0bc7968ce08e9021b55b503
hibbahagh <hibbahagh@gmail.com>
2018-06-12 17:47:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "my name is Hibba"
 end
+
+
+ get '/hometown' do 
+   "my hometown is libya"
+end 
+
+ get '/favorite-song' do 
+   "my favorite song is new lights"
+end 
+
+end 
+
